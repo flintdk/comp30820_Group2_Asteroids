efac3b29598a25015d56572a38b491f2e741e916
flintdk <92164637+flintdk@users.noreply.github.com>
2022-03-14 18:43:58 +0000

    22/03/14 TK; Created a dev branch and just did some initial setup

---
+++ src/comp30820/group2/asteroids/Asteroids.java
@@ -0,0 +1,11 @@
+package comp30820.group2.asteroids;
+
+import javafx.application.Application;
+
+public class Asteroids extends Application {
+	
+	public static void main(String[] args)
+	{
+	}
+
+}
---
+++ src/module-info.java
@@ -0,0 +1,11 @@
+module comp30820_group2_asteroids {
+	requires transitive javafx.graphics;
+	requires javafx.controls;
+	requires javafx.fxml;
+	
+	opens comp30820.group2.asteroids to javafx.fxml;
+	opens comp30820.group2.demoJFX to javafx.fxml;
+	
+	exports comp30820.group2.asteroids;
+	exports comp30820.group2.demoJFX;
+}
